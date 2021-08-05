9606ce54ddc48a0d58791e3633dd0197b494953c
itskegnh <64617857+itskegnh@users.noreply.github.com>
2021-08-05 14:01:24 +1000

    Update script.js

--- script.js
+++ script.js
@@ -8,11 +8,21 @@ function save() {
   localStorage.setItem("admin", admin);
 }
 
+function check_bg() {
+  if (theme === "default") {document.body.style.backgroundImage = "url('https://img.itch.zone/aW1nLzY2MzA2MzcucG5n/original/CI6uUc.png')";}
+  else if (theme === "rainbow") {document.body.style.backgroundImage = "linear-gradient(to right, orange , yellow, green, cyan, blue, violet)";}
+  else {document.body.style.backgroundImage = "url('https://media.tenor.com/images/52c80bffe2f2675700e4397d25071ae3/tenor.gif')";}
+}
+
 function load() {
   if (localStorage.getItem("admin") === "true") {
     admin = true;
   } else {
     admin = false;
+  if (!!localStorage.getItem("theme")) {
+    theme = localStorage.getItem("theme");
+  } else {
+    theme = "default";
 }}
 
 load()
