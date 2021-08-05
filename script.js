04d33e567b1715226e451c207fb8182ad741d481
itskegnh <64617857+itskegnh@users.noreply.github.com>
2021-08-05 14:04:21 +1000

    Update script.js

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ function save() {
 function check_bg() {
   if (theme === "default") {document.body.style.backgroundImage = "url('https://img.itch.zone/aW1nLzY2MzA2MzcucG5n/original/CI6uUc.png')";}
   else if (theme === "rainbow") {document.body.style.backgroundImage = "linear-gradient(to right, orange , yellow, green, cyan, blue, violet)";}
+  else if (theme === "dark") {document.body.style.backgroundImage = "linear-gradient(grey, grey)";}
   else {document.body.style.backgroundImage = "url('https://media.tenor.com/images/52c80bffe2f2675700e4397d25071ae3/tenor.gif')";}
 }
 
