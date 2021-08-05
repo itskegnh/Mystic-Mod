240fcd947fad48ce8078c9b8e543931868a97630
itskegnh <64617857+itskegnh@users.noreply.github.com>
2021-08-05 14:08:29 +1000

    Update script.js

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function save() {
 function check_bg() {
   if (theme === "default") {document.body.style.backgroundImage = "url('https://img.itch.zone/aW1nLzY2MzA2MzcucG5n/original/CI6uUc.png')";}
   else if (theme === "rainbow") {document.body.style.backgroundImage = "linear-gradient(to right, orange , yellow, green, cyan, blue, violet)";}
-  else if (theme === "dark") {document.body.style.backgroundImage = "#707070";}
+  else if (theme === "dark") {document.body.style.backgroundImage = "url('https://media.discordapp.net/attachments/791818284370231318/872692442040774707/unknown.png')";}
   else {document.body.style.backgroundImage = "url('https://media.tenor.com/images/52c80bffe2f2675700e4397d25071ae3/tenor.gif')";}
 }
 
